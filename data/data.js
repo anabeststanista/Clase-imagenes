b5bf268786e2af6fbdcd9c34055370fca713439f
anabeststanista <105441874+anabeststanista@users.noreply.github.com>
2022-09-08 16:50:11 -0500

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,38 @@
+export const data =
+    [
+        {
+            "id": 1,
+            "name": "Sulli",
+            "image": "/imagenes/Sulli.jpg"
+        },
+    
+
+    
+        {
+            "id": 2,
+            "name": "Taylor Swift",
+            "image": "/imagenes/Taylor Swift.jpg"
+        },
+
+    
+
+        {
+            "id": 3,
+            "name": "Marina",
+            "image": "/imagenes/Marina.jpg"
+        },
+
+        {
+            "id": 4,
+            "name": "BlackPink",
+            "image": "/imagenes/Bp.jpg"
+        },
+
+        {
+            "id": 5,
+            "name": "Twice",
+            "image": "/imagenes/Twice.jpg"
+        },
+
+
+    ]
